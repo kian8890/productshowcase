6cfbcb3f732162bc0bb875b498eaaf5f667de8d9
kian8890 <41864431+kian8890@users.noreply.github.com>
2025-04-18 08:33:54 +0300

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,10 +1,14 @@
 from django.apps import AppConfig
-from django.contrib.auth.models import User
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
     def ready(self):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')
+        from django.contrib.auth.models import User
+
+        try:
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')
+        except Exception:
+            pass
