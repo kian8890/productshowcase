e22d2bfc12eddf5657e04d8801e64cf8fd7eabe5
kian8890 <41864431+kian8890@users.noreply.github.com>
2025-04-18 06:41:25 +0300

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add/', views.product_create, name='product_add'),
+    path('edit/<int:pk>/', views.product_update, name='product_edit'),
+    path('delete/<int:pk>/', views.product_delete, name='product_delete'),
+]
 
