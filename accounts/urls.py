9d2ce92f4ce00bacc0a8dad3c1327ce3ad25927c
kian8890 <41864431+kian8890@users.noreply.github.com>
2025-04-18 06:36:32 +0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,1 +1,10 @@
-.
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('delete_user/<int:user_id>/', views.delete_user, name='delete_user'),
+]
